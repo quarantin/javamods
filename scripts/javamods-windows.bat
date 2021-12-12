d56ab851e34b01f30a80c1b9b90aabcb24ec83e3
Corentin Delorme <codelorme@gmail.com>
2021-12-12 11:36:27 +0100

    Update script javamods-windows.bat: Bootstrap JavaMods

--- scripts/javamods-windows.bat
+++ scripts/javamods-windows.bat
@@ -1,6 +1,9 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
+SET JAVA=.\jre64\bin\java.exe
 SET _JAVA_OPTIONS=
-SET PZ_CLASSPATH=.;.\*;.\javamods\*
-".\jre64\bin\java.exe" -Djava.awt.headless=true -Dzomboid.steam=1 -Dzomboid.znetlog=1 -XX:-CreateCoredumpOnCrash -XX:-OmitStackTraceInFastThrow -XX:+UseZGC -Xms1800m -Xmx2048m -Djava.library.path=./win64/;./ -cp %PZ_CLASSPATH% zombie.javamods.Main
+SET MAIN=zombie.javamods.Main
+FOR /F "tokens=*" %a in (`%JAVA% -classpath javamods.jar %MAIN% -bootstrap`) do SET JAVAMODS_CLASSPATH=%a
+SET CLASSPATH=.;.\*;%JAVAMODS_CLASSPATH%
+%JAVA% -Djava.awt.headless=true -Dzomboid.steam=1 -Dzomboid.znetlog=1 -XX:-CreateCoredumpOnCrash -XX:-OmitStackTraceInFastThrow -XX:+UseZGC -Xms1800m -Xmx2048m -Djava.library.path=./win64/;./ -cp %CLASSPATH% %MAIN%
 PAUSE
