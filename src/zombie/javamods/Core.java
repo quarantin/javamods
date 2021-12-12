ce7d431dc464ebfe46b027ef3469db8bafe0135c
Corentin Delorme <codelorme@gmail.com>
2021-12-12 16:42:29 +0100

    Update class Core.java: Add server flag to know if we're running server or client

--- src/zombie/javamods/Core.java
+++ src/zombie/javamods/Core.java
@@ -14,6 +14,7 @@ import zombie.gameStates.MainScreenState;
 public class Core {
 
 	public static boolean debug;
+	public static boolean server;
 	private static KahluaTable env;
 
 	public static Class<?> getZomboidMainClass() {
