5c1f8f73a3c722e2fa07579fcffbb5dedcc6e0ba
Corentin Delorme <codelorme@gmail.com>
2021-12-12 13:35:06 +0100

    Update script javamods-windows.bat: Fix JAVAMODS_CLASSPATH

--- scripts/javamods-windows.bat
+++ scripts/javamods-windows.bat
@@ -3,7 +3,7 @@
 SET JAVA=.\jre64\bin\java.exe
 SET _JAVA_OPTIONS=
 SET MAIN=zombie.javamods.Main
-FOR /F "tokens=*" %a in (`%JAVA% -classpath javamods.jar %MAIN% -bootstrap`) do SET JAVAMODS_CLASSPATH=%a
+FOR /F "tokens=*" %%a in (`%JAVA% -classpath javamods.jar %MAIN% -bootstrap`) do SET JAVAMODS_CLASSPATH=%%a
 SET CLASSPATH=.;.\*;%JAVAMODS_CLASSPATH%
 %JAVA% -Djava.awt.headless=true -Dzomboid.steam=1 -Dzomboid.znetlog=1 -XX:-CreateCoredumpOnCrash -XX:-OmitStackTraceInFastThrow -XX:+UseZGC -Xms1800m -Xmx2048m -Djava.library.path=./win64/;./ -cp %CLASSPATH% %MAIN%
 PAUSE
