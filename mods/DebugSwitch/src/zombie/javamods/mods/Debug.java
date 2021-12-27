c29baa3017a5c0789eb1efc4ee6f75e6f646d884
Corentin Delorme <codelorme@gmail.com>
2021-12-27 08:04:14 +0100

    Add new java mod `DebugSwitch`: Add global function `setDebug(boolean)` to enabled or disabled debug mode from a mod.

---
+++ mods/DebugSwitch/src/zombie/javamods/mods/Debug.java
@@ -0,0 +1,23 @@
+package zombie.javamods.mods;
+
+import java.util.Arrays;
+import java.util.List;
+
+import se.krka.kahlua.integration.annotations.LuaMethod;
+
+import zombie.javamods.mod.GlobalFunctionJavaMod;
+import zombie.core.Core;
+
+
+public class Debug extends GlobalFunctionJavaMod {
+
+	@Override
+	public List<Object> getObjectsWithGlobalFunctions() {
+		return Arrays.asList(this);
+	}
+
+	@LuaMethod(name = "setDebug", global = true)
+	public static void setDebug(boolean debug) {
+		Core.bDebug = debug;
+	}
+}
