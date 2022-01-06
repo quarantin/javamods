713d5e0d167b5c610b4aad04ca6dcf0ba1179604
Corentin Delorme <codelorme@gmail.com>
2022-01-07 00:10:29 +0100

    Update Patcher.java: Fix class to allow more than one patch per class

--- src/javamods/Patcher.java
+++ src/javamods/Patcher.java
@@ -5,11 +5,13 @@ import java.lang.instrument.IllegalClassFormatException;
 
 import java.security.ProtectionDomain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtConstructor;
 import javassist.CtMethod;
 
 import javamods.Patch;
@@ -17,37 +19,85 @@ import javamods.Patch;
 
 public class Patcher implements ClassFileTransformer {
 
-	HashMap<String, Patch> patches;
+	HashMap<String, List<Patch>> patches;
 
 	public Patcher(List<Patch> patches) {
+
+		List<Patch> patchList;
+
 		this.patches = new HashMap<>();
 		for (Patch patch : patches) {
+
 			String className = patch.getTargetClass().replace(".", "/");
-			this.patches.put(className, patch);
+
+			if (!this.patches.containsKey(className))
+				this.patches.put(className, new ArrayList<>());
+
+			patchList = this.patches.get(className);
+			patchList.add(patch);
 		}
 	}
 
-	public byte[] transform(ClassLoader loader, String className, Class<?> targetClass, ProtectionDomain proectionDomain, byte[] classBytes) {
+	private void patchConstructor(CtClass ctClass, Patch patch) throws Exception {
+
+		String methodBody = patch.getMethodBody();
+		CtConstructor constructor = ctClass.getDeclaredConstructor(null);
+
+		if (patch.insertAfter())
+			constructor.insertAfter(methodBody);
+
+		else
+			constructor.insertBefore(methodBody);
+	}
+
+	private void patchMethod(CtClass ctClass, Patch patch) throws Exception {
+
+		String methodBody = patch.getMethodBody();
+		CtMethod method = ctClass.getDeclaredMethod(patch.getTargetMethod());
+
+		if (patch.insertAfter())
+			method.insertAfter(methodBody);
+
+		else
+			method.insertBefore(methodBody);
+	}
+
+	private byte[] apply(String className, List<Patch> patches) throws Exception {
+
+		ClassPool classPool = ClassPool.getDefault();
+		CtClass ctClass = classPool.get(className.replace("/", "."));
+
+		for (Patch patch : patches) {
+
+			String targetClass = patch.getTargetClass();
+			String targetMethod = patch.getTargetMethod();
+
+			if (targetClass.equals(targetMethod) || targetClass.endsWith("." + targetMethod))
+				patchConstructor(ctClass, patch);
+
+			else
+				patchMethod(ctClass, patch);
+		}
+
+		byte[] bytecode = ctClass.toBytecode();
+		ctClass.detach();
+
+		return bytecode;
+
+	}
+
+	public byte[] transform(ClassLoader loader, String className, Class<?> classs, ProtectionDomain proectionDomain, byte[] classBytes) {
 
 		byte[] bytecode = classBytes;
 
-		Patch patch = patches.get(className);
-		if (patch != null) {
+		List<Patch> patches = this.patches.get(className);
+		if (patches != null) {
 
 			Log.info("Patching class " + className);
 
 			try {
-				ClassPool classPool = ClassPool.getDefault();
-				CtClass cc = classPool.get(patch.getTargetClass());
-				CtMethod method = cc.getDeclaredMethod(patch.getTargetMethod());
 
-				if (patch.insertAfter())
-					method.insertAfter(patch.getMethodBody());
-				else
-					method.insertBefore(patch.getMethodBody());
-
-				bytecode = cc.toBytecode();
-				cc.detach();
+				bytecode = apply(className, patches);
 			}
 			catch (Exception error) {
 				Log.error(error);
